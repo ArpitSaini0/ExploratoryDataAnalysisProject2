174725316da7a4d19d5288b7ee6d83868deb62f7
ArpitSaini0 <arpit.saini.mit@gmail.com>
2022-04-01 02:28:17 +0530

    commit all

---
+++ plot3.R
@@ -0,0 +1,17 @@
+SCC <- data.table::as.data.table(x = readRDS(file = "data/Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "data/summarySCC_PM25.rds"))
+
+library("data.table")
+library("ggplot2")
+
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
